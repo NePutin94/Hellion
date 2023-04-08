996c3ada6f766f3b245ad78c845e11737428d746
NePutin <dimkap314@gmail.com>
2023-04-08 14:13:36 +0300

    Update RenderSystem.h

--- include/vulkan/RenderSystem.h
+++ include/vulkan/RenderSystem.h
@@ -15,6 +15,7 @@
 #include <tracy/TracyVulkan.hpp>
 #include "../core/Profiling.h"
 #include "../HCamera.h"
+#include "../HModel.h"
 
 namespace Hellion
 {
@@ -22,13 +23,13 @@ namespace Hellion
     {
     public:
         RenderSystem(HDevice& device, vk::RenderPass renderPass, HSwapChain& swapchain)
-                : device{device}
+                : device{device}, model{device}
         {
+            loadModel();
             createPipelineLayout();
             createPipeline(renderPass, swapchain);
-            loadModel();
-            createVertexBufferVma();
-            createIndexBufferVma();
+            //createVertexBufferVma();
+            //createIndexBufferVma();
         }
 
         ~RenderSystem()
@@ -51,12 +52,58 @@ namespace Hellion
             float time = std::chrono::duration<float, std::chrono::seconds::period>(currentTime - startTime).count();
 
             UniformBufferObject ubo{};
-            ubo.model = glm::rotate(glm::mat4(1.0f), glm::radians(90.0f), glm::vec3(0.0f, 0.0f, 1.0f));
+            ubo.model = glm::mat4 {1};
             ubo.view = camera.getViewMatrix();//glm::lookAt(glm::vec3(2.0f, 2.0f, 2.0f), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 0.0f, 1.0f));
             ubo.proj = camera.getProjectionMatrix();//glm::perspective(glm::radians(45.0f), width / (float) height, 0.1f, 10.0f);
             ubo.proj[1][1] *= -1;
             ubo.time = time * 15;
             memcpy(uboBuffers[currentFrame]->getMappedMemory(), &ubo, sizeof(ubo));
+        }
+
+        void renderNode(vk::CommandBuffer& buffer, uint32_t currentFrame, tracy::VkCtx* tracyCtx, Node* node)
+        {
+            if(node->mesh)
+            {
+                for(auto& primitive: node->mesh->getPrimitives())
+                {
+                    const std::vector<vk::DescriptorSet> descriptorsets = {
+                            globalDescriptorSets[currentFrame],
+                            node->mesh->getDescriptorSet(),
+                    };
+                    buffer.bindDescriptorSets(vk::PipelineBindPoint::eGraphics, pipelineLayout, 0, descriptorsets, {});
+                    buffer.drawIndexed(primitive.getIndexCount(), 1, primitive.getFirstIndex(), 0, 0);
+                }
+            }
+            for(auto& child: node->children)
+            {
+                renderNode(buffer, currentFrame, tracyCtx, child);
+            }
+
+//            if(node->mesh)
+//            {
+//                // Render mesh primitives
+//                for(auto primitive: node->mesh->getPrimitives())
+//                {
+//                    const std::vector<vk::DescriptorSet> descriptorsets = {
+//                            globalDescriptorSets[currentFrame],
+//                            node->mesh->getDescriptorSet(),
+//                    };
+//
+//                    buffer.bindDescriptorSets(vk::PipelineBindPoint::eGraphics, pipelineLayout, 0, descriptorsets, {});
+//
+//                    if(primitive.isHasIndices())
+//                    {
+//                        buffer.drawIndexed(primitive.getIndexCount(), 1, primitive.getFirstIndex(), 0, 0);
+//                    } else
+//                    {
+//                        buffer.draw(primitive.getIndexCount(), primitive.getVertexCount(), 1, 0);
+//                    }
+//                }
+//            }
+//            for(auto& child: node->children)
+//            {
+//                renderNode(buffer, currentFrame, tracyCtx, child);
+//            }
         }
 
     private:
@@ -67,6 +114,8 @@ namespace Hellion
             alignas(16) glm::mat4 proj;
             alignas(16) float time;
         };
+        HModel model;
+
 
         void createVertexBufferVma()
         {
@@ -99,11 +148,19 @@ namespace Hellion
             HELLION_ZONE_PROFILING()
             texture = HTexture::createTextureFromFile(device, TEXTURE_PATH.c_str());
 
+            size_t meshCount = model.getMeshCount();
+
             globalPool =
                     HDescriptorPool::Builder(device)
                             .setMaxSets(HSwapChain::MAX_FRAMES_IN_FLIGHT)
                             .addPoolSize(vk::DescriptorType::eUniformBuffer, HSwapChain::MAX_FRAMES_IN_FLIGHT)
-                            .addPoolSize(vk::DescriptorType::eCombinedImageSampler, HSwapChain::MAX_FRAMES_IN_FLIGHT)
+                                    // .addPoolSize(vk::DescriptorType::eCombinedImageSampler, HSwapChain::MAX_FRAMES_IN_FLIGHT)
+                            .build();
+            meshPool =
+                    HDescriptorPool::Builder(device)
+                            .setMaxSets(meshCount * HSwapChain::MAX_FRAMES_IN_FLIGHT)
+                            .addPoolSize(vk::DescriptorType::eUniformBuffer, meshCount * HSwapChain::MAX_FRAMES_IN_FLIGHT)
+                                    // .addPoolSize(vk::DescriptorType::eCombinedImageSampler, HSwapChain::MAX_FRAMES_IN_FLIGHT)
                             .build();
 
             uboBuffers.resize(HSwapChain::MAX_FRAMES_IN_FLIGHT);
@@ -124,6 +181,17 @@ namespace Hellion
                             .addBinding(1, vk::DescriptorType::eCombinedImageSampler, vk::ShaderStageFlagBits::eFragment)
                             .build();
 
+
+            meshLayout =
+                    HDescriptorSetLayout::Builder(device)
+                            .addBinding(0, vk::DescriptorType::eUniformBuffer, vk::ShaderStageFlagBits::eVertex)
+                            .build();
+
+            for(auto& node: model.getNodes())
+            {
+                Node::setupNodeDescriptorSet(node, *meshPool, device, *meshLayout);
+            }
+
             globalDescriptorSets.resize(HSwapChain::MAX_FRAMES_IN_FLIGHT);
             for(int i = 0; i < globalDescriptorSets.size(); i++)
             {
@@ -135,9 +203,13 @@ namespace Hellion
                         .build(globalDescriptorSets[i]);
             }
 
+            const std::vector<vk::DescriptorSetLayout> setLayouts = {
+                    renderSystemLayout->getDescriptorSetLayout(), meshLayout->getDescriptorSetLayout()
+            };
+
             vk::PipelineLayoutCreateInfo pipelineLayoutInfo{};
-            pipelineLayoutInfo.setLayoutCount = 1;
-            pipelineLayoutInfo.pSetLayouts = &renderSystemLayout->getDescriptorSetLayout();
+            pipelineLayoutInfo.setLayoutCount = setLayouts.size();
+            pipelineLayoutInfo.pSetLayouts = setLayouts.data();
             pipelineLayoutInfo.pushConstantRangeCount = 0;
             pipelineLayoutInfo.pPushConstantRanges = nullptr;
 
@@ -159,53 +231,54 @@ namespace Hellion
         void loadModel()
         {
             HELLION_ZONE_PROFILING()
-            tinyobj::attrib_t attrib;
-            std::vector<tinyobj::shape_t> shapes;
-            std::vector<tinyobj::material_t> materials;
-            std::string warn, err;
-
-            if(!tinyobj::LoadObj(&attrib, &shapes, &materials, &warn, &err, MODEL_PATH.c_str()))
-            {
-                throw std::runtime_error(warn + err);
-            }
-
-            std::unordered_map<HVertex, uint32_t> uniqueVertices{};
-
-            for(const auto& shape: shapes)
-            {
-                for(const auto& index: shape.mesh.indices)
-                {
-                    HVertex vertex{};
-
-                    vertex.pos = {
-                            attrib.vertices[3 * index.vertex_index + 0],
-                            attrib.vertices[3 * index.vertex_index + 1],
-                            attrib.vertices[3 * index.vertex_index + 2]
-                    };
-
-                    vertex.texCoord = {
-                            attrib.texcoords[2 * index.texcoord_index + 0],
-                            1.0f - attrib.texcoords[2 * index.texcoord_index + 1]
-                    };
-
-                    vertex.color = {1.0f, 1.0f, 1.0f};
-
-                    if(uniqueVertices.count(vertex) == 0)
-                    {
-                        uniqueVertices[vertex] = static_cast<uint32_t>(vertices.size());
-                        vertices.push_back(vertex);
-                    }
-
-                    indices.push_back(uniqueVertices[vertex]);
-                }
-            }
+            model.loadFromFile("../Data/Models/gLTF/SimpleMeshes.gltf");
+//            tinyobj::attrib_t attrib;
+//            std::vector<tinyobj::shape_t> shapes;
+//            std::vector<tinyobj::material_t> materials;
+//            std::string warn, err;
+//
+//            if(!tinyobj::LoadObj(&attrib, &shapes, &materials, &warn, &err, MODEL_PATH.c_str()))
+//            {
+//                throw std::runtime_error(warn + err);
+//            }
+//
+//            std::unordered_map<HVertex, uint32_t> uniqueVertices{};
+//
+//            for(const auto& shape: shapes)
+//            {
+//                for(const auto& index: shape.mesh.indices)
+//                {
+//                    HVertex vertex{};
+//
+//                    vertex.pos = {
+//                            attrib.vertices[3 * index.vertex_index + 0],
+//                            attrib.vertices[3 * index.vertex_index + 1],
+//                            attrib.vertices[3 * index.vertex_index + 2]
+//                    };
+//
+//                    vertex.texCoord = {
+//                            attrib.texcoords[2 * index.texcoord_index + 0],
+//                            1.0f - attrib.texcoords[2 * index.texcoord_index + 1]
+//                    };
+//
+//                    vertex.color = {1.0f, 1.0f, 1.0f};
+//
+//                    if(uniqueVertices.count(vertex) == 0)
+//                    {
+//                        uniqueVertices[vertex] = static_cast<uint32_t>(vertices.size());
+//                        vertices.push_back(vertex);
+//                    }
+//
+//                    indices.push_back(uniqueVertices[vertex]);
+//                }
+//            }
         }
 
         HDevice& device;
         std::unique_ptr<HPipeline> pipeline;
         vk::PipelineLayout pipelineLayout;
+        std::unique_ptr<HDescriptorPool> meshPool;
         std::unique_ptr<HDescriptorPool> globalPool;
-
         std::unique_ptr<HTexture> texture;
 
         std::vector<std::unique_ptr<HBuffer>> uboBuffers;
@@ -216,6 +289,7 @@ namespace Hellion
         std::unique_ptr<HBuffer> indexBuffer;
         std::vector<vk::DescriptorSet> globalDescriptorSets;
         std::unique_ptr<HDescriptorSetLayout> renderSystemLayout;
+        std::unique_ptr<HDescriptorSetLayout> meshLayout;
     };
 
 } // Hellion
